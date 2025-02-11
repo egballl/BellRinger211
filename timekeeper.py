fe0d49a53ee63e4e8bc70e555ca5a73d74a65713
egballl <148280195+egballl@users.noreply.github.com>
2025-02-11 14:56:31 -0800

    Add files via upload
    incomplete, marks a number of dates 14 - 11 for 3 days and some amount of hours

---
+++ timekeeper.py
@@ -0,0 +1,13 @@
+import time
+import datetime
+currents = datetime.datetime.now()
+currentdays = int(currents.strftime('%d'))
+currentdd = int(currents.strftime('%H'))
+print("Current Time:", currents)
+feb14 = datetime.datetime(2025, 2, 14, 0)
+feb14track = int(feb14.strftime('%d'))
+feb14trackss = int(feb14.strftime('%H'))
+feb14math = feb14track - currentdays
+print("Days until Feb 14: ", feb14math)
+hours = feb14trackss - currentdd
+print("hours until feb14:", hours)
